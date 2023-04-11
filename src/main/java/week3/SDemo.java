6639dcf4fd77fee9174aa4f265ee3fe6d86dcf0e
Daria.Deriuhina <Daria.Deriuhina@medeanalytics.com>
2023-04-11 06:53:01 -0700

    static variables

---
+++ src/main/java/week3/SDemo.java
@@ -0,0 +1,30 @@
+package week3;
+
+public class SDemo {
+    public static void main(String[] args) {
+
+        StaticDemo ob1 = new StaticDemo();
+        StaticDemo ob2 = new StaticDemo();
+
+        ob1.x = 10;
+        ob2.x = 20;
+
+        System.out.println("ob1.x and ob2.x are independent. \nob1.x: " + ob1.x + "\nob2.x: " + ob2.x);
+        System.out.println();
+
+        System.out.println("Static variable y is shared");
+        System.out.println("Assign StaticDemo.y value 19");
+        StaticDemo.y = 19;
+
+        System.out.println("ob1.sum: " + ob1.sum());
+        System.out.println("ob2.sum: " + ob2.sum());
+        System.out.println();
+
+        System.out.println("Change StaticDemo.y to 100");
+        StaticDemo.y = 100;
+
+        System.out.println("ob1.sum: " + ob1.sum());
+        System.out.println("ob2.sum: " + ob2.sum());
+        System.out.println();
+    }
+}
---
+++ src/main/java/week3/StaticDemo.java
@@ -0,0 +1,10 @@
+package week3;
+
+public class StaticDemo {
+    int x; //usual variable
+    static int y; //static variable
+
+    int sum(){
+        return x + y;
+    }
+}
